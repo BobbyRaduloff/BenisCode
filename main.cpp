e1e85d47edbfb6d7a782ea122eba20b3d35cacf7
Bobby Raduloff <blank100kappa@gmail.com>
2016-12-22 20:23:19 +0200

    Added decoding and cleaned up some useless code... Probably last code related commit.

--- main.cpp
+++ main.cpp
@@ -6,54 +6,41 @@
 
 using namespace std;
 
-bool isStringOnlySpace(string& str){
-	if(str.find_first_not_of(' ') != string::npos)
-		return false;
-	return true;
-}
-
-void printVecString(vector<string>& vec){
-	for(int i = 0; i < vec.size(); i++){
-		cout << vec[i] << endl;
-	}
-}
-
 void splitString(string& str, vector<string>& vec){
-	int start = 0;
 	string newString = "";
-	for(int i = 0; i < str.length(); i++){
-		if(str[i] != ' '){
+	for(int i = 0; i < str.length(); i++)
+		if(str[i] != ' ')
 			newString += str[i];
-		}else{
-			if(!isStringOnlySpace(str)){
-				vec.push_back(newString);
-				newString = "";
-			}
+		else{
+			vec.push_back(newString);
+			newString = "";
 		}
-	}
-	for(int i = 0; i < vec.size(); i++){
-		cout << vec[i] << endl;
-	}
+	vec.push_back(newString);
 }
 
 void encodeStringToNums(string& str, vector<unsigned int>& nums){
-	for(int i = 0; i < str.size(); i++){
-		if(str[i] == ' '){
+	bool isCaps = false;
+	for(int i = 0; i < str.size(); i++)
+		if(str[i] == ' ')
 			nums.push_back(0);
-		}else if(str[i] >= 'a' && str[i] <= 'z'){
-			nums.push_back(str[i] - 'a' + 1);	
-		}else if(str[i] == ':'){
-			nums.push_back(27);
-		}else if(str[i] == ';'){
-			nums.push_back(28);
-		}else if(str[i] == '.'){
+		else if(str[i] >= 'a' && str[i] <= 'z'){
+			if(isCaps){
+				nums.push_back(28);
+				isCaps = false;
+			}
+			nums.push_back(str[i] - 'a' + 1);
+		}else if(str[i] >= 'A' && str[i] <= 'Z'){
+			if(!isCaps){
+				nums.push_back(27);
+				isCaps = true;
+			}
+			nums.push_back(str[i] - 'a' + 1);
+		}else if(str[i] == '.')
 			nums.push_back(29);
-		}else if(str[i] == '?'){
+		else if(str[i] == '?')
 			nums.push_back(30);
-		}else if(str[i] == '!'){
+		else if(str[i] == '!')
 			nums.push_back(31);
-		}
-	}
 }
 
 void encodeNumsToBenis(vector<unsigned int>& nums, string& encoded){
@@ -61,17 +48,47 @@ void encodeNumsToBenis(vector<unsigned int>& nums, string& encoded){
 		string currentBenis = "benis";
 		bitset<5> bits(nums[i]);
 		string bitsString = bits.to_string<char, string::traits_type, string::allocator_type>();
-		for(int j = 0; j < currentBenis.length(); j++){
+		for(int j = 0; j < 5; j++)
 			if(bitsString[j] == '1')
 				currentBenis[j] = toupper(currentBenis[j]);
-		}
 		encoded += currentBenis;
-		if(i < nums.size() - 1){
+		if(i < nums.size() - 1)
 			encoded += ' ';
-		}
 	}
 }
 
+void decodeWords(vector<string>& vec, string& str){
+	bool isCaps = false;
+	for(int i = 0; i < vec.size(); i++){
+		bitset<5> bits;
+		bits.set();
+		for(int j = 0; j < vec[i].length(); j++)
+			if(islower(vec[i][j]))
+				bits[j] = 0;
+		bitset<5> bits2;
+		bits2 = bits;
+		for(int j = 0; j < 5; j++)
+			bits[j] = bits2[4-j];
+		unsigned long num = bits.to_ulong();
+		if(num == 0)
+			str += ' ';
+		else if(num >= 1 && num <=26)
+			if(!isCaps)
+				str += (char) num + 'a' - 1;
+			else
+				str += (char) num + 'A' - 1;
+		else if(num == 27)
+			isCaps = true;
+		else if(num == 28)
+			isCaps = false;
+		else if(num == 29)
+			str += '.';
+		else if(num == 30)
+			str += '?';
+		else if(num == 31)
+			str += '!';
+	}
+}
 
 int main(){
 	string input;
@@ -81,16 +98,19 @@ int main(){
 	cout << "[D]ecode or [E]ncode: ";
 	getline(cin, input);
 	if(tolower(input[0]) == 'd'){
-
+		cout << "Text to decode: ";
+		getline(cin, input);
+		cout << input << endl;
+		splitString(input, words);
+		decodeWords(words, output);
+		cout << "Decoded: " << output << endl;
 	}else if(tolower(input[0]) == 'e'){
 		cout << "Text to encode: ";
 		getline(cin, input);
-		transform(input.begin(), input.end(), input.begin(), ::tolower);
 		encodeStringToNums(input, nums);
 		encodeNumsToBenis(nums, output);
 		cout << "Encoded: " << output << endl;
-	}else{
+	}else
 		cout << "Invalid input! Bye..." << endl;
-	}
 	return 0;
 }
